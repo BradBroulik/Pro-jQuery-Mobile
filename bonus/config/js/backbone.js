0306ac0501154c63b8a1c5971fbcf4bca4b5e043
BradBroulik <brad.broulik@gmail.com>
2013-01-06 10:16:58 -0600

    Added backbone config example

---
+++ bonus/config/js/backbone.js
@@ -0,0 +1,13 @@
+$(document).bind("mobileinit", function(){
+	// Disable JQM page navigation, let backbone handle routing
+	$.mobile.ajaxEnabled = false;
+	$.mobile.linkBindingEnabled = false;
+	$.mobile.hashListeningEnabled = false;
+	$.mobile.pushStateEnabled = false;
+	
+	// Must manually remove pages from DOM when JQM isn't handling page navigation
+	$(document).on('pagehide', 'div[data-role="page"]',
+	function (event, ui) {
+			$(event.currentTarget).remove();
+	});
+});
